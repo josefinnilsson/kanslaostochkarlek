d20b3379af8d103a7d7648e47990ff5af926b3ab
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2018-12-22 22:00:54 +0100

    Handle users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  root 'static_pages#home'
+  get 'sessions/new'
+  get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
