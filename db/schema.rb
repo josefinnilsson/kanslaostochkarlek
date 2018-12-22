fddea994339bdcd5e0dbf04709767f74415ed940
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2018-12-22 19:43:13 +0100

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181222180554) do
+ActiveRecord::Schema.define(version: 20181222183847) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 20181222180554) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
