e97736892a88714578b9405151e8dde03f5cb625
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-01-01 17:47:13 +0530

    Change databse structure

--- db/migrate/20190101121254_create_ingredients.rb
+++ db/migrate/20190101121254_create_ingredients.rb
@@ -2,6 +2,8 @@ class CreateIngredients < ActiveRecord::Migration[5.1]
   def change
     create_table :ingredients do |t|
       t.string :name
+      t.integer :amount
+      t.string :measure
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181222180554) do
+ActiveRecord::Schema.define(version: 20190101121254) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "measures", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "recipe_ingredients", force: :cascade do |t|
-    t.integer "recipe_id"
-    t.integer "ingredient_id"
-    t.integer "measure_id"
     t.integer "amount"
+    t.string "measure"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -53,6 +40,15 @@ ActiveRecord::Schema.define(version: 20181222180554) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
